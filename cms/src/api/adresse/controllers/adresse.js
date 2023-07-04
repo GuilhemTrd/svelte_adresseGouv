7c2fd6e6c73bbeb5e523a2598b51d69ec3e73971
guilhemtrd <guilhem.trd@gmail.com>
2023-07-04 15:16:53 +0200

    works : CMS

---
+++ cms/src/api/adresse/controllers/adresse.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * adresse controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::adresse.adresse');
---
+++ cms/src/api/adresse/services/adresse.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * adresse service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::adresse.adresse');
